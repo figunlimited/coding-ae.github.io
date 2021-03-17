600f7b730c5deef11429426827f311950adafb60
Muhammad Faqih <58163699+figunlimited@users.noreply.github.com>
2021-03-17 16:29:54 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//turorial smooth scroll
+// event pada saat link di klik
+
+$('.page-scroll').on('click', function() {
+
+    //ambil isi href
+    var tujuan = $(this).attr('href');
+    
+    //tangkap elemen ybs
+    var elemenTujuan = $(tujuan);
+    
+    //pindahkan scroll
+    $('body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    })
+
+   
+
+});
